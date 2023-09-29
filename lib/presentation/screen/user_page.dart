a3962107d93b3df04cf54ca7ff3d106e4836301e
Ilya Manenkov <i.manenkov@cryptology.com>
2023-09-30 02:06:27 +0600

    wantKeepAlive user page

--- lib/presentation/screen/user_page.dart
+++ lib/presentation/screen/user_page.dart
@@ -4,11 +4,17 @@ import 'package:skillbox_app/presentation/bloc/user_bloc/user_bloc.dart';
 import 'package:skillbox_app/presentation/bloc/user_bloc/user_state.dart';
 import 'package:skillbox_app/presentation/widget/user_card.dart';
 
-class UserPage extends StatelessWidget {
+class UserPage extends StatefulWidget {
   const UserPage({super.key});
 
   @override
+  State<UserPage> createState() => _UserPageState();
+}
+
+class _UserPageState extends State<UserPage> with AutomaticKeepAliveClientMixin {
+  @override
   Widget build(BuildContext context) {
+    super.build(context);
     return BlocBuilder<UserBloc, UserState>(builder: ((context, state) {
       if (state is UserInitialState || state is UserLoadingState) {
         return const Center(child: CircularProgressIndicator());
@@ -31,4 +37,7 @@ class UserPage extends StatelessWidget {
       }
     }));
   }
+
+  @override
+  bool get wantKeepAlive => true;
 }
